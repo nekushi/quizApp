19be0714b021995a978cf045223f606dc62cb2d1
nekushi <122607874+nekushi@users.noreply.github.com>
2024-04-19 20:52:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const third = document.querySelector("label[for=d]")
+const btn = document.querySelector(".show-ans")
+
+btn.addEventListener("click", function() {
+    third.style.backgroundColor = "#0f0"
+})
